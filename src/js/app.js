58daf86989807b533465de277c72efc5a75927dc
dasharubakova <dasharubakova@yandex.ru>
2023-05-12 16:30:34 +0300

    added breadcrumbs & about block

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,45 @@
 import * as flsFunctions from "./modules/functions.js";
 
+import "./modules/import-jquery.js";
+import "@fancyapps/fancybox";
+import "slick-carousel";
+import mixitup from 'mixitup';
+
 // flsFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+$(function () {
+
+  $('.header__btn').on('click', function () {
+    $('.rightside-menu').removeClass('rightside-menu--close');
+  })
+  $('.rightside-menu__close').on('click', function () {
+    $('.rightside-menu').addClass('rightside-menu--close');
+  })
+
+  $('.top__slider').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+  });
+
+  if($('.gallery__inner').length) {
+    var mixer = mixitup('.gallery__inner', {
+      load: {
+        filter: ".living",
+      }
+    });
+  }
+
+  // $(".about__popup-play").fancybox({
+  //   type:'swf',
+  //   allowfullscreen: 'true',
+  // })
+  
+})
+
+flsFunctions.highlightMenuLink();
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,28 +1,15 @@
 import $ from "jquery";
 import "slick-carousel";
-import mixitup from 'mixitup';
 
-$(function () {
-
-  $('.header__btn').on('click', function () {
-    $('.rightside-menu').removeClass('rightside-menu--close');
-  })
-  $('.rightside-menu__close').on('click', function () {
-    $('.rightside-menu').addClass('rightside-menu--close');
-  })
-
-  $('.top__slider').slick({
-    dots: true,
-    arrows: false,
-    fade: true,
-    autoplay: true,
-  });
-
-
-  var mixer = mixitup('.gallery__inner', {
-    load: {
-      filter: ".living",
+export let highlightMenuLink = function() {
+  $('.menu .menu__list-link').each(function() {
+    const location = window.location.href;
+    const link = this.href;
+    if(location == link) {
+      $(this).addClass('--active');
     }
-  });
-  
-})
+    else {
+      $(this).removeClass('--active');
+    }
+  })
+}
